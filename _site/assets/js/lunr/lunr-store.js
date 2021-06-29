3de6aa937de33fb527bd2ef7227aa9354abd6db6
jskim1215 <kjs01092505762@gmail.com>
2021-06-29 23:38:32 +0900

    예상 프로덕트 개발 순서 포스팅

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -17,9 +17,9 @@ var store = [{
         "url": "/calander/first-review/",
         "teaser": null
       },{
-        "title": "1일차 : 일정 계획 세팅",
-        "excerpt":"오늘 할 일    Github page 준비하기  저번 방학 때, 자동매매 프로그램 개발 과정을 기록하겠다고 만들었다가 일주일도 안되서 버린 페이지를 재활용해서 블로그를 만들었다. 따라 할 사람들은 Jekyll/Github 블로그만들기를 참고해서 따라하면 된다.   러프하게 프로덕트 개발 계획 세우기  뭘 할 줄 모르겠을 때 가장 효율이 좋은 행동, 그건 모방이라고 생각한다. 우연히 패스트벤쳐스 TextBook 프로그램 단체 카카오톡 방에서 처음 Disquiet이라는 사이트를 접하게 됬고, 여기서 매거진 파트에서 프로덕트 개발 여정을 기록한 것을 보고 나랑 비슷한 행동을 했고, 이미 투자까지 받을 정도의 결과를 보여줘서 나의 모방 상대로 정했다.  ","categories": ["Calander"],
+        "title": "예상 프로덕트 개발 순서",
+        "excerpt":"   기획  제품 개발의 내적 동기  제품 아이디어 정리  가설 및 가설 검증 전략 세팅   개발  최소한의 기능 정리  기능 실현을 위한 노코드 툴 List화  스토리보드 &amp; 와이어프레임 작성  제품 제작  마케팅 &amp; 검증  홍보물 제작 및 홍보채널 List화  베타 테스트  가설 검증 데이터 결과 정리  ","categories": ["Calander"],
         "tags": ["기획"],
-        "url": "/calander/overall-plan/",
+        "url": "/calander/summary/",
         "teaser": null
       }]
